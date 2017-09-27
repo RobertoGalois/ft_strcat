87286d60d6eb1e05987462701a40c6144bbb48ca
RobertoGalois <victor.vague@live.fr>
2017-09-27 20:20:19 +0200

    Create ft_clean_strcat.c

---
+++ ft_clean_strcat.c
@@ -0,0 +1,29 @@
+#include    <stdio.h>
+
+char*   ft_strcat(char* dest, char* src)
+{
+        int     count;
+        int     size_dest;
+
+        /*
+        ** getting the size of the string in char* dest
+        */
+        count = 0;
+        size_dest = -1; 
+        while (dest[++size_dest] != '\0');
+
+        /*
+        ** Add str2 at the end of str1
+        */
+        while (src[count] != '\0')
+        {
+            dest[(size_dest + count)] = src[count]; 
+            count++;
+        }
+
+        /*
+        ** add the final \0
+        */
+        dest[(size_dest + count)] = '\0';
+        return (dest);
+}
